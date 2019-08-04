0a07c127e46e74c6552d0141f0f8d4e539d179dc
vinibispo <vini.bispo015@gmail.com>
2019-08-03 23:04:24 -0300

    git merger complete

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-async function start(){
-    const {user, pass, repo} = require('./robots/input')
-    const browser = require('./robots/browser')
-    await browser(user, pass, repo)
-}
-start()
+const {spawn, exec} = require('child_process')
+const gc = exec('git', ['checkout', 'master'])
+const gm = spawn('git', ['merge', process.argv[2]])
+gm.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`)
+})
+
+gm.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`)
+})
+
+gm.on('close', (code) => {
+  console.log(`child process exited with code ${code}`)
+})
